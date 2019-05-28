535019fba93592a56a33fe0032b6ac6184c2f32f
changliao1025 <changliao1025@gmail.com>
2019-05-27 23:07:18 -0700

    update library

--- toolbox/date/day_of_year.py
+++ toolbox/date/day_of_year.py
@@ -1,6 +1,6 @@
-import julian
-import datetime
-
+#import julian
+#import datetime
+from jdcal import gcal2jd, jd2gcal
 
 def day_of_year(iYear_in, iMonth_in, iDay_in):
 
@@ -23,11 +23,15 @@ def day_of_year(iYear_in, iMonth_in, iDay_in):
           return -1
 
           
-    first_day_of_year = datetime.datetime(iYear_in, 1, 1)
-    current_day_of_year = datetime.datetime(iYear_in, iMonth_in, iDay_in)
-    lJulian_start = julian.to_jd(first_day_of_year, fmt='jd')
-    lJulian_end = julian.to_jd(current_day_of_year, fmt='jd')
+    #first_day_of_year = datetime.datetime(iYear_in, 1, 1)
+    #current_day_of_year = datetime.datetime(iYear_in, iMonth_in, iDay_in)
+
+    #lJulian_start = julian.to_jd(first_day_of_year, fmt='jd')
+    #lJulian_end = julian.to_jd(current_day_of_year, fmt='jd')
+
+    lJulian_start = gcal2jd(iYear_in, 1, 1)
+    lJulian_end =  gcal2jd(iYear_in, iMonth_in, iDay_in)
 
 
-    dayofyear = lJulian_end-lJulian_start + 1
+    dayofyear = int (lJulian_end[1] - lJulian_start[1] ) + 1
     return dayofyear
