099feece0742dca45a8c8e11bb0ca9d5c852aebd
changliao1025 <changliao1025@gmail.com>
2019-07-31 17:22:32 -0700

    color

--- toolbox/color/choose_n_color.py
+++ toolbox/color/choose_n_color.py
@@ -1,9 +1,6 @@
 import numpy as np
 def choose_n_color(nColor_in, pColorMap_in):
-    kn = np.arange(nColor_in)
-    aColor=[]
-    for k in kn:
-        h,binEdges=np.histogram(data1[k])
-        color=cmap(float(k)/kn.max())
-        aColor.append[color]
+    kn = np.arange(nColor_in) 
+
+    aColor = [pColorMap_in(float(k)/kn.max()) for k in kn]
     return aColor
