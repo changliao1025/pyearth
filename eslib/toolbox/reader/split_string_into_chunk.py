ad14ba107dd64accbeba9f679c5f6db55006e94b
Chang Liao <changliao.climate@gmail.com>
2019-12-30 17:11:36 -0800

    fix error

--- eslib/toolbox/reader/split_string_into_chunk.py
+++ eslib/toolbox/reader/split_string_into_chunk.py
@@ -1,6 +1,6 @@
 def split_string_into_chunk(sString_in,iChunk_size_in=None): # we assume here that iChunk_size_in is an int and > 0
     iTotel_size = len(sString_in)
-    if iChunk_size_in in not None:
+    if iChunk_size_in is not None:
         iChunk_size = iChunk_size_in
     else:
         iChunk_size = 10
