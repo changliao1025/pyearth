7f0194c871350a9c5452187ce8c2d49bb87791dd
Chang Liao <changliao.climate@gmail.com>
2020-06-10 13:08:32 -0700

    add day in month

---
+++ eslib/toolbox/date/day_in_month.py
@@ -0,0 +1,13 @@
+from jdcal import gcal2jd, jd2gcal
+
+def day_in_month(iYear_in, iMonth_in):
+    lJulian_start = gcal2jd(iYear_in, iMonth_in, 1)
+
+    if iMonth_in < 12:
+        lJulian_end =  gcal2jd(iYear_in, iMonth_in+1, 1)
+    else:
+        lJulian_end =  gcal2jd(iYear_in+1, 1, 1)
+
+
+    dayinmon = int (lJulian_end[1] - lJulian_start[1] ) 
+    return dayinmon
