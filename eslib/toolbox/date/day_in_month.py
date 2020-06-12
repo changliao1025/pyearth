9364364e816250d28a3a5a3edbfc5ed16dd4c9d7
Chang Liao <changliao.climate@gmail.com>
2020-06-12 10:52:29 -0700

    add date feature to consider leap year

--- eslib/toolbox/date/day_in_month.py
+++ eslib/toolbox/date/day_in_month.py
@@ -1,6 +1,9 @@
 from jdcal import gcal2jd, jd2gcal
-
-def day_in_month(iYear_in, iMonth_in):
+import os, sys
+sSystem_paths = os.environ['PATH'].split(os.pathsep)
+sys.path.extend(sSystem_paths)
+from eslib.toolbox.date.leap_year import leap_year
+def day_in_month(iYear_in, iMonth_in, iFlag_leap_year_in = None):
     lJulian_start = gcal2jd(iYear_in, iMonth_in, 1)
 
     if iMonth_in < 12:
@@ -8,6 +11,17 @@ def day_in_month(iYear_in, iMonth_in):
     else:
         lJulian_end =  gcal2jd(iYear_in+1, 1, 1)
 
+    if iFlag_leap_year_in == 0:
+        if leap_year(iYear_in):
+            if iMonth_in == 2:
+                dayinmon = 28
+            else:
+                dayinmon = int (lJulian_end[1] - lJulian_start[1] ) 
+        else:
 
-    dayinmon = int (lJulian_end[1] - lJulian_start[1] ) 
+            dayinmon = int (lJulian_end[1] - lJulian_start[1] ) 
+    else:
+
+
+        dayinmon = int (lJulian_end[1] - lJulian_start[1] ) 
     return dayinmon
