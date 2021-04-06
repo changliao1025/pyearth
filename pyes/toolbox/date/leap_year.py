d067ab4dbdcbe63ba06e5769bcb13f549ddcabb2
changliao1025 <changliao1025@outlook.com>
2021-04-06 10:43:52 -0700

    remove old folder

--- pyes/toolbox/date/leap_year.py
+++
@@ -1,9 +0,0 @@
-def leap_year(y):
-    if y % 400 == 0:
-        return True
-    if y % 100 == 0:
-        return False
-    if y % 4 == 0:
-        return True
-    else:
-        return False
--- pyes/toolbox/geometry/calculate_hexagon_area.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-def calculate_hexagon_area(effe_edge):
-    area = 1.5 * np.sqrt(3.0) * effe_edge
-    return area
