baa916487f786eae96c68860360530b57a530cd2
changliao1025 <changliao1025@outlook.com>
2021-07-15 15:11:40 -0700

    add world2pixel

---
+++ pyearth/gis/gdal/world2Pixel.py
@@ -0,0 +1,14 @@
+def world2Pixel(geoMatrix, x, y):
+    """
+    Uses a gdal geomatrix (gdal.GetGeoTransform()) to calculate
+    the pixel location of a geospatial coordinate
+    """
+    ulX = geoMatrix[0]
+    ulY = geoMatrix[3]
+    xDist = geoMatrix[1]
+    yDist = geoMatrix[5]
+    rtnX = geoMatrix[2]
+    rtnY = geoMatrix[4]
+    pixel = int((x - ulX) / xDist)
+    line = int((ulY - y) / xDist)
+    return (pixel, line)
