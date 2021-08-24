802fe67b7e395e78aa238948c5fc9c19c180d33f
changliao1025 <changliao1025@outlook.com>
2021-08-24 13:25:31 -0700

    repro

--- pyearth/gis/location/calculate_distance_based_on_lon_lat.py
+++ pyearth/gis/location/calculate_distance_based_on_lon_lat.py
@@ -13,5 +13,6 @@ def calculate_distance_based_on_lon_lat(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1 
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
-    r = 6371000 # Radius of earth in kilometers. Use 3956 for miles
+    #r = 6371000 # Radius of earth in kilometers. Use 3956 for miles
+    r = 6378137.0
     return c * r
